a3eba3afa8a048754b75396425c751ae85a4f5b7
SaiChuquipiondo <saichuquipiondopinchi@gmail.com>
2026-01-06 09:55:19 -0500

    cambio para localmente

--- Proyecto_DSSL_SaiChuquipiondo/frontend/src/environments/environment.ts
+++ Proyecto_DSSL_SaiChuquipiondo/frontend/src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://tesisapi.up.railway.app/api',
   wsUrl: 'https://tesisapi.up.railway.app'
+  // apiUrl: 'http://localhost:3000/api',
+  // wsUrl: 'http://localhost:3000'
 };
 
